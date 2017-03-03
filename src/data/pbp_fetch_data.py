fdee6ad3eaa42a4a70db88324327754a6cf37167
mdgoldberg <matt.goldberg7@gmail.com>
2017-03-02 20:12:16 -0500

    explicit about number of partitions in pbp_fetch_data, edits to slurm script

--- src/data/pbp_fetch_data.py
+++ src/data/pbp_fetch_data.py
@@ -48,7 +48,7 @@ def process_boxscore_id(boxscore_id):
 def fetch_pbp_data_year(year):
     season = nba.Season(year)
     boxscore_ids = season.get_schedule().boxscore_id.values
-    bsids_bag = db.from_sequence(boxscore_ids)
+    bsids_bag = db.from_sequence(boxscore_ids, npartitions=16)
     dfs_bag = bsids_bag.map(process_boxscore_id)
     dfs = dfs_bag.compute()
     filt_dfs = filter(None, dfs)
