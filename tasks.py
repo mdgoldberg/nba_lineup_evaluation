d0e80b43549e2e6f290d16c09b539cb3034d059d
mdgoldberg <matt.goldberg7@gmail.com>
2017-03-07 01:12:28 -0500

    set up compiling macros for thesis

--- tasks.py
+++ tasks.py
@@ -5,3 +5,21 @@ import invoke
 
 dotenv_path = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_path)
+
+thesis_dir = os.path.join(os.environ['PROJ_DIR'], 'reports', 'thesis')
+
+@invoke.task
+def compile(ctx):
+    start_dir = os.getcwd()
+    os.chdir(thesis_dir)
+    ctx.run('xelatex thesis')
+    ctx.run('bibtex thesis')
+    ctx.run('xelatex thesis')
+    ctx.run('xelatex thesis')
+    ctx.run('mv thesis.log .logged')
+    ctx.run('latexmk -c')
+    os.chdir(start_dir)
+
+@invoke.task
+def view(ctx):
+    ctx.run('open {}/thesis.pdf'.format(thesis_dir))
