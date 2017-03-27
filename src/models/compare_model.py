63c40c6de1c2bd73086a30227979c839215737b8
mdgoldberg <matt.goldberg7@gmail.com>
2017-03-27 01:09:07 -0400

    added compare_model, updated train_model

--- src/models/compare_model.py
+++ src/models/compare_model.py
@@ -4,14 +4,10 @@ import logging.config
 import multiprocessing as mp
 import os
 
-import dask
-from dask import delayed
 import dotenv
 import numpy as np
 import pandas as pd
-from sklearn import (base, decomposition, ensemble, linear_model,
-                     metrics, model_selection, preprocessing)
-import xgboost as xgb
+from sklearn import linear_model, metrics
 
 from sportsref import nba
 
@@ -24,10 +20,9 @@ n_jobs = os.environ.get('SLURM_NTASKS', mp.cpu_count()-1)
 
 seasons_train = range(2007, 2015)
 seasons_test = range(2015, 2017)
+seasons = seasons_train + seasons_test
 
-dr_est = decomposition.PCA() # TODO: fill in
-reg_est = linear_model.LinearRegression() # TODO: fill in
-
+reg_est = linear_model.LinearRegression()
 
 def get_logger():
     logging.config.fileConfig(
@@ -38,52 +33,49 @@ def get_logger():
 
 
 def _design_matrix_one_season(args):
-    logger = get_logger()
-    logger.info('starting _design_matrix_one_season')
-    lineups, sub_profs, sub_rapm, sub_hm_off = args
+    lineups, sub_orapm, sub_drapm, sub_hm_off = args
     hm_lineups = lineups.ix[:, nba.pbp.HM_LINEUP_COLS]
     aw_lineups = lineups.ix[:, nba.pbp.AW_LINEUP_COLS]
-    rp_val = sub_rapm.loc['RP']
-    hm_rapm = hm_lineups.applymap(lambda p: sub_rapm.get(p, rp_val))
-    aw_rapm = aw_lineups.applymap(lambda p: sub_rapm.get(p, rp_val))
-    hm_rapm_idxs = np.argsort(-hm_rapm, axis=1)
-    aw_rapm_idxs = np.argsort(-aw_rapm, axis=1)
-    hm_lineups = hm_lineups.apply(
-        lambda r: r[hm_rapm_idxs.loc[r.name]].values, axis=1
-    )
-    aw_lineups = aw_lineups.apply(
-        lambda r: r[aw_rapm_idxs.loc[r.name]].values, axis=1
-    )
+
     hm_off_df = pd.concat((hm_lineups[sub_hm_off], aw_lineups[sub_hm_off]),
                            axis=1)
     aw_off_df = pd.concat((aw_lineups[~sub_hm_off], hm_lineups[~sub_hm_off]),
                            axis=1)
-    cols = ['{}_player{}'.format(tm, i)
-            for tm in ['off', 'def'] for i in range(1, 6)]
+    off_cols = ['off_player{}'.format(i) for i in range(1, 6)]
+    def_cols = ['def_player{}'.format(i) for i in range(1, 6)]
+    cols = off_cols + def_cols
     hm_off_df.columns = cols
     aw_off_df.columns = cols
+
     merged_df = pd.concat((hm_off_df, aw_off_df))
+    off_df = merged_df[off_cols]
+    def_df = merged_df[def_cols]
+
+    rp_oval = sub_orapm.loc['RP']
+    off_rapm = off_df.applymap(lambda p: sub_orapm.get(p, rp_oval))
+    off_rapm = pd.DataFrame(
+        np.flip(off_rapm.apply(np.sort, axis=1).values, 1),
+        columns=off_cols
+    )
+    rp_dval = sub_drapm.loc['RP']
+    def_rapm = def_df.applymap(lambda p: sub_drapm.get(p, rp_oval))
+    def_rapm = pd.DataFrame(
+        np.flip(def_rapm.apply(np.sort, axis=1).values, 1),
+        columns=def_cols
+    )
+
+    merged_df = pd.concat((off_rapm, def_rapm), axis=1)
     n_hm_off = len(hm_off_df)
     merged_df['hm_off'] = [i < n_hm_off for i in range(len(merged_df))]
-    for col in cols:
-        sub_profs.columns = [
-            '{}_{}'.format(i, col) for i in range(sub_profs.shape[1])
-        ]
-        merged_df = pd.merge(
-            merged_df, sub_profs, how='left',
-            left_on=col, right_index=True
-        ).fillna(sub_profs.loc['RP'])
 
-    merged_df.drop(cols, axis=1, inplace=True)
     return merged_df
 
 
-def create_design_matrix(lineups, profiles, seasons, rapm, hm_off):
-    prof_cols = profiles.columns
+def create_design_matrix(lineups, orapm, drapm, seasons, hm_off):
     seasons_uniq = np.unique(seasons)
-    pool = mp.Pool(4)
+    pool = mp.Pool(min(4, n_jobs))
     args_to_eval = [
-        (lineups[seasons == s], profiles.xs(s, level=1), rapm.xs(s, level=1),
+        (lineups[seasons == s], orapm.xs(s, level=1), drapm.xs(s, level=1),
          hm_off[seasons == s])
         for s in seasons_uniq
     ]
@@ -100,10 +92,8 @@ profile_dfs = [
     helpers.get_profiles_data(season) for season in seasons
 ]
 profile_df = pd.concat(profile_dfs)
-rapm = profile_df.loc[:, ['orapm', 'drapm']].sum(axis=1)
-profiles_scaled = (
-    profile_df.groupby(level=1).transform(lambda x: (x - x.mean()) / x.std())
-)
+orapm = profile_df['orapm']
+drapm = profile_df['drapm']
 del profile_df, profile_dfs
 
 # load and process the play-by-play data for regression
@@ -133,49 +123,40 @@ hm_off_test = poss_hm_off[~pbp_is_train]
 y_train = y[pbp_is_train]
 y_test = y[~pbp_is_train]
 
-# split profiles data into train and test
-prof_is_train = profiles_scaled.index.get_level_values(1).isin(seasons_train)
-profiles_train = profiles_scaled[prof_is_train]
-profiles_test = profiles_scaled[~prof_is_train]
-
 # fit model on training data
-logging.info('starting to fit DR model')
-latent_train = dr_est.fit_transform(profiles_train)
-logging.info('done fitting DR model')
-logging.info('starting create_design_matrix for train')
+# logging.info('starting create_design_matrix for train')
 X_train = create_design_matrix(
-    lineups_train, latent_train, poss_year_train, rapm, hm_off_train
+    lineups_train, orapm, drapm, poss_year_train, hm_off_train
 )
-logging.info('done with create_design_matrix for train')
-logging.info('starting to fit regression model')
+# logging.info('done with create_design_matrix for train')
+# logging.info('starting to fit regression model')
 reg_est.fit(X_train, y_train)
-logging.info('done fitting regression model')
+# logging.info('done fitting regression model')
 
 # score model on test data
-logging.info('using DR model to transform test')
-latent_test = dr_est.transform(profiles_test)
-logging.info('done transforming test profiles')
-logging.info('starting create_design_matrix for test')
+# logging.info('starting create_design_matrix for test')
 X_test = create_design_matrix(
-    lineups_test, latent_test, poss_year_test, rapm, hm_off_test
+    lineups_test, orapm, drapm, poss_year_test, hm_off_test
 )
-logging.info('done with create_design_matrix for test')
-logging.info('predicting on X_test')
+# logging.info('done with create_design_matrix for test')
+# logging.info('predicting on X_test')
 predictions = reg_est.predict(X_test)
-logging.info('done predicting on X_test')
-logging.info('generating metrics')
+# logging.info('done predicting on X_test')
+# logging.info('generating metrics')
 
 rmse = metrics.mean_squared_error(y_test, predictions)
 r2 = metrics.r2_score(y_test, predictions)
 mae = metrics.median_absolute_error(y_test, predictions)
 
-logging.info('RMSE: {}'.format(rmse))
-logging.info('R^2: {}'.format(r2))
-logging.info('median abs err: {}'.format(mae))
+# logging.info('RMSE: {}'.format(rmse))
+# logging.info('R^2: {}'.format(r2))
+# logging.info('median abs err: {}'.format(mae))
 
 perf_metrics = pd.Series({
     'rmse': rmse,
     'r2': r2,
     'median_abs_error': mae
 })
-perf_metrics.to_csv('data/models/final_perf_metrics.csv')
+perf_metrics.to_csv('data/models/basic_perf_metrics.csv')
+
+
